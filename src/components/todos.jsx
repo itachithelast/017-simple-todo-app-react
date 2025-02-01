246be484bd9e3be73242f2380e5f76942fed86b7
Hamed <h.namazi91@gmail.com>
2025-02-01 18:31:23 +0330

    updating status completed

--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -3,41 +3,54 @@ import TasksContext from "../assets/context";
 
 class ToDO extends Component {
   static contextType = TasksContext;
-  loadPendings = () => {
-    return this.context.tasks.filter((element) =>element.isPending).map(element=>
-         (
-          <div
-            id={element.id}
-            className="item-pending row align-items-center border-bottom border-dark "
-          >
-            <i className="fa-regular fa-square col-1"></i>
-            <span className="col">
-              {element.name}
-            </span>
-            <i className="fa-solid fa-trash-can col-1"></i>
-          </div>
-        ))
-      }
 
-  loadCompleted = ()=>{
-    return this.context.tasks.filter(element=>!element.isPending).map(
-      element=>(
+  handleStatusChange = (el) => {
+    const id = Number(el.parentElement.id);
+    const updatedTasks = this.context.tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, isPending: !task.isPending };
+        }
+        return task;
+      })
+    this.context.updateTasks(updatedTasks)
+    };
+  handleEdit = null;
+  HandleDelete = null;
+
+  loadTasks = (isPending) => {
+    return this.context.tasks
+      .filter((element) => element.isPending === isPending)
+      .map((element) => (
         <div
-        id={element.id}
-        className="item-complete row align-items-center border-bottom border-dark "
-      >
-        <i className="fa-regular fa-square-check col-1"></i>
-        <span className="col text-decoration-line-through text-inactive">
-          {element.name}
-        </span>
-        <i className="fa-solid fa-trash-can col-1"></i>
-      </div>
-      )
-    )
-  }
+          key={element.id}
+          id={element.id}
+          className={`item-${
+            isPending ? "pending" : "complete"
+          } row align-items-center border-bottom border-dark`}
+        >
+          <i
+            onClick={(e) => this.handleStatusChange(e.target)}
+            className={`fa-regular ${
+              isPending ? "fa-square" : "fa-square-check"
+            } col-1`}
+          ></i>
+          <span
+            onClick={(e) => this.handleEdit(e.target)}
+            className={`col ${
+              !isPending ? "text-decoration-line-through text-inactive" : ""
+            }`}
+          >
+            {element.name}
+          </span>
+          <i
+            onClick={(e) => this.HandleDelete(e.target)}
+            className="fa-solid fa-trash-can col-1"
+          ></i>
+        </div>
+      ));
+  };
 
   render() {
-    console.log(this.context);
     return (
       <div className="container mt-5 p-5 rounded-4">
         <h3>Task Tracker</h3>
@@ -49,9 +62,8 @@ class ToDO extends Component {
           />
           <button className="enter-key rounded"></button>
         </form>
-        <div className="pending mt-5 p-3">{this.loadPendings()}</div>
-        <div className="completed px-3">{this.loadCompleted()}
-        </div>
+        <div className="pending mt-5 p-3">{this.loadTasks(true)}</div>
+        <div className="completed px-3">{this.loadTasks(false)}</div>
       </div>
     );
   }
